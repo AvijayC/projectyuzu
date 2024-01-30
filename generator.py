e37b5de95e7957782b53b067d51158513934de33
Avijay Chakravorti <avijay.c@gmail.com>
2024-01-29 17:40:44 -0800

    more hardcoded stars

--- generator.py
+++ generator.py
@@ -1,13 +1,13 @@
 # Template: {name: 'Star1', pos: [-4, 5, 4], oid: 1}
 from random import random;
 
-mult = 500
+mult = 100
 def generateRng():
     randomValue = random() - 0.5;
     return randomValue * mult;
 
 def run():
-    for i in range(6, 500):
+    for i in range(6, 100):
         print("{" + f"name: \"Star{i}\", pos: [{generateRng()}, {generateRng()}, {generateRng()}], oid: {i}" + "},")
 
 
