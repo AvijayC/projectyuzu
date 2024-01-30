5194e1b2c2eb68f6de573c3f6fb8661884fec1a1
Avijay Chakravorti <avijay.c@gmail.com>
2024-01-29 17:02:22 -0800

    more hardcoded stars

---
+++ src/app/UIHandler.ts
@@ -0,0 +1,16 @@
+// Create event listeners for the UI elements.
+
+import Core from "./Core.js";
+
+export class UIHandler {
+    core: Core;
+    constructor(core: Core) {
+        this.core = Core;
+    }
+
+    initSidebar() {
+        let sidebar = document.getElementById("sidebar");
+        let sidebarBtn = document.getElementById("sidebarBtn");
+    }
+
+}
